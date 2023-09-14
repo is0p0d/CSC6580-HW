3a2feed5565617d3a9108d285e765f82605a868e
Jimbo <57813877+is0p0d@users.noreply.github.com>
2023-09-14 13:43:38 -0500

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -6,76 +6,104 @@
 ; a program that writes a user specified string N number of times
 ; usage: hello N string
 
-extern atoi
-extern puts
+; There has to be a cleaner way to do this, but brain no worky after RGA work....
 
 global main
 
-section .text
+extern atoi
+extern puts
+extern _GLOBAL_OFFSET_TABLE_
 
-; NOTE: eax is the exit status
-
+    section .text
 main:
     push    rbp
     mov     rbp, rsp
-    push    rdi
+    cmp     rdi, 3
+    jl      _arg_exit
+    push    r12
+
+;num convert
+
     push    rax
     push    rbx
-    push    rcx
-    pushf
+    push    rdi
+    push    rsi
+    push    r9
 
-    cmp     rdi, 2
-    jne     _arg_exit
 
-    push    qword [rsi+8]         ;put value on stack
-    call    atoi
-    pop     rcx             ;put result in counter
-    add     rsp, 8          ;clean up the stack
+    lea     rbx, [rel _GLOBAL_OFFSET_TABLE_]
+    mov     rdi, [rsi+8]
+    lea     rsi, [rel main]
+    mov     r9, atoi wrt ..got
+    call    [rbx+r9]
+    mov     r12, rax ; move into counter
+
+    pop     r9
+    pop     rsi
+    pop     rdi
+    pop     rbx 
+    pop     rax
 
 _print:
-    push    qword [rsi+16]
-    call    puts
-    add     rsp, 8
-    dec     rcx
-    
-    cmp     rcx, 0
-    jne     _print
 
-_done:
-    mov     rbx, 0          ;set exit status to 0
-    
-    popf
-    pop     rcx
-    pop     rbx
-    pop     rax
+    push    rax
+    push    rbx
+    push    rdi
+    push    rsi
+    push    r9
+
+    lea     rbx, [rel _GLOBAL_OFFSET_TABLE_]
+    mov     rdi, [rsi+16]
+    lea     rsi, [rel main]
+    mov     r9, puts wrt ..got
+
+    cmp     r12, 0
+    jle     _done_print
+    call    [rbx+r9]
+    dec     r12
+
+    pop     r9
+    pop     rsi
     pop     rdi
+    pop     rbx 
+    pop     rax
+    jmp     _print
+
+_done_print:
+    mov     eax, 0 ; successful return
+    pop     r9
+    pop     rsi
+    pop     rdi
+    pop     rbx 
+    pop     rax 
+    pop     r12
     leave
     ret
 
 _arg_exit:
-    push    arg_error
-    call    puts
-    add     rsp, 8
-    mov     rbx, 1          ;set exit status to 1
-    
-    popf
-    pop     rcx
-    pop     rbx
-    pop     rax
+    push    rbx
+    push    rdi
+    push    rsi
+    push    r9
+
+    lea     rbx, [rel _GLOBAL_OFFSET_TABLE_]
+    lea     rdi, [rel mg_argerr]
+    lea     rsi, [rel main]
+    mov     r9, puts wrt ..got
+    call    [rbx+r9]
+    mov     eax, 1 ; error return
+
+    pop     r9
+    pop     rsi
     pop     rdi
+    pop     rbx
     leave
     ret
 
 
-section .data
-arg_error:
-    db      "ERROR: You must uspply a number and a string as arguments", 0
+    section .data nowrite
+mg_argerr:
+    db     "ERROR: You must supply a number and a string as arguments.", 0
+    
 
-section .note.GNU-stack noexec
-
-
-; COMPILATION ERROR: 
-; nasm -f elf64 "hello.asm" -o "hello.o" && gcc -o "hello" "hello.o" -fPIE
-; /usr/bin/ld: hello.o: relocation R_X86_64_32S against `.data' can not be used when making a PIE object; recompile with -fPIE
-; /usr/bin/ld: failed to set dynamic section sizes: bad value
-; collect2: error: ld returned 1 exit status
+section .not.GNU-stack noexec
